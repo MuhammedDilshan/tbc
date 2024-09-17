0b0bb64e064afea10914c128d2e98bd3f442e60a
dilshanmambadan <dilshanmambadan@gmail.com>
2024-09-17 16:57:48 +0530

    dilshan

--- src/Componetns/Header/Header.jsx
+++ src/Componetns/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Header.css";
 import { IoMdSearch } from "react-icons/io";
 import { HiMenuAlt2 } from "react-icons/hi";
@@ -6,6 +6,18 @@ import { assets } from "../../Assets/assets";
 import { MdOutlineShoppingBag } from "react-icons/md";
 
 const Header = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 480);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 480);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div>
       <header>
@@ -13,9 +25,11 @@ const Header = () => {
           <h6>THE BLACK CROW</h6>
         </div>
         <div className="bottomHeader">
+          {isMobile && <MdOutlineShoppingBag />}
           <h1>
             <img src={assets.Logo} alt="" />
           </h1>
+
           <div className="right_section">
             <form action="">
               <input type="text" placeholder="Find your City" />
